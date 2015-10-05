5b3e5b43f4cebb3f302e57343071a3ef85c0246a
Michael D. Misamore <m.misamore@gmail.com>
2015-10-05 18:36:11 -0400

    Record progress

--- src/core.js
+++ src/core.js
@@ -1,12 +1,13 @@
+export const INITIAL_STATE = Map();
 import {List,Map} from 'immutable';
 
 export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
-export function vote(state, entry) {
-  return state.updateIn(
-    ['vote', 'tally', entry],
+export function vote(voteState, entry) {
+  return voteState.updateIn(
+    ['tally', entry],
     0,
     tally => tally + 1
   );
@@ -24,10 +25,16 @@ function getWinners(vote) {
 
 export function next(state) {
   const entries = state.get('entries').concat(getWinners(state.get('vote')));
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
+  if (entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else { 
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
 }
 
 
